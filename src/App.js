4f18d2474f70b21628f9069ac6922173a560fdb2
Jtavora <jtavora09@gmail.com>
2023-09-21 19:34:29 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Card">
+        <div className="Card-inner">
+        <img
+            src={require('./image.png').default}
+            className="Card-image"
+          />
+
+          <div className="Card-info">
+            <h2>CJ</h2>
+            <p>Piloto</p>
+            <p>San Andreas</p>
+            <p>Email: cj@GTA.com</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
